0357a37e8aef1a6e17a08cb16f7dcd58b54d55e2
IlchenkoSergey <siarhei.ilchanka@regula.us>
2025-04-10 15:34:11 +0300

    SP-22497 - update cdn sample

--- react-ts/globals.d.ts
+++
@@ -1,9 +0,0 @@
-import { IdvWebComponent } from '@regulaforensics/idv-capture-web';
-
-declare global {
-  namespace React.JSX {
-    interface IntrinsicElements {
-      'idv-flow': React.DetailedHTMLProps<HTMLAttributes<IdvWebComponent>, IdvWebComponent>;
-    }
-  }
-}
--- react-ts/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const element = document.getElementById('root');
-const root = createRoot(element as HTMLElement);
-
-root.render(<App />);

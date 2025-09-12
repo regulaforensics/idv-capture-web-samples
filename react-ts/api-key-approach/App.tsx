c7e51a5f4573e064f2dfec74a30858d77261702d
Ihar Lapitski <ihar.lapitski@regula.us>
2025-09-12 14:17:38 +0300

    SP-25233 - Added Api Key samples

---
+++ react-ts/api-key-approach/App.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useRef } from "react";
+import { IdvIntegrationService, IdvMessageEvent } from "@regulaforensics/idv-capture-web";
+import { FaceIdv } from "@regulaforensics/idv-face";
+import { DocumentIdv } from "@regulaforensics/idv-document";
+
+function App() {
+  const service = useRef<IdvIntegrationService | null>(null);
+
+  const listener = (event: IdvMessageEvent) => {
+    console.log(event);
+  }
+
+  useEffect(() => {
+    service.current = new IdvIntegrationService();
+    service.current.sessionRestoreMode = true;
+    service.current.eventListener = listener;
+    const serviceRun = async () => {
+      const initResult = await service.current?.initialize({
+        modulesConfig: {
+          docreader: {
+            devLicense: 'Base64License',
+          }
+        },
+        includedModules: [FaceIdv, DocumentIdv],
+      });
+      if (initResult?.error) {
+        console.log(initResult.error);
+        return;
+      }
+  
+      const configureResult = await service.current?.configure({
+        baseUrl: "", // set host
+        apiKey: "", // set api key
+        ttl: 86400,  // set time to live
+      });
+      console.log(configureResult);
+      if (configureResult?.error) {
+        console.log(configureResult.error);
+        return;
+      }
+      const prepareResult = await service.current?.prepareWorkflow({
+        workflowId: "", // set workflow id
+      });
+      if (prepareResult?.error) {
+        console.log(prepareResult.error);
+        return;
+      }
+      const metadata = { test: true };
+      const startWorkflowResult = await service.current?.startWorkflow({
+        metadata: metadata,
+        locale: 'en'
+      });
+      if (startWorkflowResult?.error) {
+        console.log(startWorkflowResult.error);
+        return;
+      }
+      console.log("WORKFLOW FINISHED :", startWorkflowResult);
+    }
+    serviceRun();
+  }, []);
+
+  return (
+    <div style={{ height: '100%', width: '100%' }}>
+      <idv-flow></idv-flow>
+    </div>
+  );
+}
+
+export default App;

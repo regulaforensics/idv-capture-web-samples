37791fc9b85c09926b137531d001c6ccf3a6471a
nina.viter <nina.viter@regula.us>
2026-08-11 15:15:54 +0300

    61616 - load font from netlify project

--- react-ts/user-pass-approach/App.tsx
+++ react-ts/user-pass-approach/App.tsx
@@ -18,7 +18,7 @@ function App() {
       const initResult = await service.current?.initialize({
         modulesConfig: {
           docreader: {
-            devLicense: 'Base64License',
+            devLicense: 'AAEAAA2Uo8qkJF0Juj8kmPlEXWRfgJJ3W2nqFSVPDVsEp53bXM7k594lE1AUUEm4HP9VquqNJSZWVFHoVO+p9bbRwJWLyYtABqs8ICXire74/3gkIy2QkoaJgZo+4FThZJRu3Rki6u7RGzRWditj4cI/XEXRrwv4TVDoJndFSRX/XE9lTC155SQcAt43CaIRI6mHzBA6ErmM6beF/zqmc0W1Aj0CENAn0vhDn2axAYpBHDYl6IViMkj9TIQbpyFPFHGU3RyZnz0UzIVfWhu3+kOkN01Ei28NtVEgVuc3bUr1Y3zvb0n7y1+i3hyvOD+PcM5qsPJrRIwlvrzp0ggcak3FgykkAQAAAAAAEGU1Doc9Y+ZdzpYD2N/N/suS+PJfrCoDA+++mJ8pva4hFTpyS87jpgRvO5LhoZRwyr7gPHq5zPTGiJtYpRpuR7uVHZmECdnQvC8IotpqYOHv8vvY9DEXypNLuydzOVpaoB0ftf6UEZif2SFlQyT0gNhDyoxA+dIZX6ROzVAfumtEuGV3l4nAet6ZMDAEc2EDO6mpiFj7XsA1CDxZI52OiGE8Rg7pCCXqmu+epOBjkDZ8U/aGQpd7joM00nMUsBI/yX+BZOg6GUaMBub5C3fEcJd4ZYXBNDgl9NmAcHkq4uGs8mbCDZIdWjQngw0dMAMXb7HQ7EZTWoMxi+XG5Sbt+ivkuxRCFsx7OziqoMwCBjg1YpLyv1zZhX/+X63NHqbezA==',
           }
         },
         includedModules: [FaceIdv, DocumentIdv],
--- react-ts/user-pass-approach/index.tsx
+++ react-ts/user-pass-approach/index.tsx
@@ -1,13 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-import BitcountSingle from "./styles/fonts/BitcountSingle-VariableFont_CRSV,ELSH,ELXP,slnt,wght.ttf";
-
-import './global.css'
-
 export async function loadFonts() {
     const fonts = await Promise.all([
-        new FontFace("Bitcount Single", `url(${BitcountSingle})`, {
+        new FontFace("Bitcount Single", `url("https://incandescent-fairy-1c2024.netlify.app/fonts/BitcountSingle-VariableFont_CRSV,ELSH,ELXP,slnt,wght.ttf")`, {
             weight: "400",
         }).load(),
     ]);
